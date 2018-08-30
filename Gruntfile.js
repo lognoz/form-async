0288116dc42e4d2d463fb9fa83dfae70ff9f6ddc
lognoz <developer@lognoz.org>
2018-08-30 00:57:06 -0400

    Refactor gzip variable

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 
-	var gzip = require( "gzip-js" )
+	var gzip = require("gzip-js");
 	var travis = process.env.TRAVIS;
 
 	grunt.initConfig({
