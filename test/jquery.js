adc656ec88cd73458fde7161a0178f5f51409473
lognoz <developer@lognoz.org>
2018-08-31 12:05:39 -0400

    Add jquery version loader

---
+++ test/jquery.js
@@ -0,0 +1,10 @@
+(function() {
+	var path = 'http://code.jquery.com/jquery-3.2.1.min.js';
+	var version = location.search.match(/[?&]jquery=(.*?)(?=&|$)/);
+
+	if (version) {
+		path = 'http://code.jquery.com/jquery-' + version[1] + '.js';
+	}
+
+	document.write('<script src="' + path + '"></script>');
+}());
