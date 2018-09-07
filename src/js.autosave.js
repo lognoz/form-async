4a69b8bfbd298e8d8f2b7e69813c55f33c815e50
lognoz <developer@lognoz.org>
2018-09-07 01:08:10 -0400

    Fix indentation in factory statement

--- src/js.autosave.js
+++ src/js.autosave.js
@@ -2,9 +2,9 @@
 	if (typeof define === 'function' && define.amd) {
 		define(['jquery'], factory);
 	} else if (typeof exports === 'object') {
-      module.exports = factory(require('jquery'));
-   } else {
-      factory(jQuery);
+		module.exports = factory(require('jquery'));
+	} else {
+		factory(jQuery);
 	}
 } (function($, window) {
 	'use strict';
