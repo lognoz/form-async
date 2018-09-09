10da73538b25b5ff4ce2b36d0ca2f2c469bc0820
lognoz <developer@lognoz.org>
2018-09-09 16:53:02 -0400

    Optimize ContextualManager()

--- src/contextual.js
+++ src/contextual.js
@@ -4,58 +4,45 @@
  * before, fail and success functions is sending as arguments, it will be
  * store in references variable.
  */
-var ContextualManager = (function() {
-	var instance, references = {
+function ContextualManager() {
+	this.references = {
 		parent: [],
 		selector: []
 	};
 
-	function ContextualManager() {
-		return {
-			watch: function(properties) {
-				properties = this.set(properties, 'parent');
-				properties = this.set(properties, 'selector');
-				return properties;
-			},
-			set: function(properties, type) {
-				var key;
-				var exist = false;
-				var list = references[type];
-				var length = list.length;
+	this.watch = function(properties) {
+		properties = this.set(properties, 'parent');
+		properties = this.set(properties, 'selector');
+		return properties;
+	},
 
-				if (type === 'selector') {
-					properties.id_selector = length;
-					references[type].push(properties);
-				} else {
-					for (key in list) {
-						if (list[key] === properties.parent[0]) {
-							properties.id_parent = parseInt(key);
-							exist = true;
-						}
-					}
+	this.set = function(properties, type) {
+		var key;
+		var exist = false;
+		var list = this.references[type];
+		var length = list.length;
 
-					if (!exist) {
-						references[type].push(properties.parent[0]);
-						properties.id_parent = length;
-					}
+		if (type === 'selector') {
+			properties.id_selector = length;
+			this.references[type].push(properties);
+		} else {
+			for (key in list) {
+				if (list[key] === properties.parent[0]) {
+					properties.id_parent = parseInt(key);
+					exist = true;
 				}
-
-				return properties;
-			},
-			get: function(id, type) {
-				return references[type][id];
 			}
-		};
-	}
 
-	return {
-		new: function(){
-			if (instance == null) {
-				instance = new ContextualManager();
+			if (!exist) {
+				this.references[type].push(properties.parent[0]);
+				properties.id_parent = length;
 			}
-			return instance;
 		}
-	};
-}());
 
-var contextual = ContextualManager.new();
+		return properties;
+	},
+
+	this.get = function(id, type) {
+		return this.references[type][id];
+	}
+}
