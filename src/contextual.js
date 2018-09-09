53c350afa0e942f7c6e9d87f61fb919e3832198a
lognoz <developer@lognoz.org>
2018-09-09 00:56:46 -0400

    Add header comment to describe how this singleton is managing

--- src/contextual.js
+++ src/contextual.js
@@ -1,3 +1,25 @@
+/**
+ * Contextual manager is a singleton that be uses to manages application
+ * references variables when developpers implements $.fn.autosave(). If
+ * before, fail and success functions is sending as arguments, it will be
+ * store in references variable.
+ *
+ *  references.before:   function
+ *  references.fail:     function
+ *  references.success:  function
+ *  references.action:   selectors action attribute
+ *  references.form:     intializator selectors
+ *  references.input:
+ *    {
+ *      action    key of references.action
+ *      form      key of references.form
+ *      name      selector name attribute
+ *      selector  selector itself
+ *      tag       selector tag type
+ *      trigger   type of trigger event
+ *      value     last saving value
+ *    }
+ */
 var ContextualManager = (function() {
 	var instance, references = {
 		before:    [],
