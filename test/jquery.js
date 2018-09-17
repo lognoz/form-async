b8f356fa19b6dc29ffc306d27e4b4b70646e79b7
lognoz <developer@lognoz.org>
2018-09-16 21:19:22 -0400

    Tests: Refactor jquery.js and cleanup useless variables

--- test/jquery.js
+++ test/jquery.js
@@ -1,10 +1,9 @@
 (function() {
-	var path = 'http://code.jquery.com/jquery-3.2.1.min.js';
-	var version = location.search.match(/[?&]jquery=(.*?)(?=&|$)/);
+	var path = 'http://code.jquery.com/jquery-3.2.1.min.js',
+	    version = location.search.match(/[?&]jquery=(.*?)(?=&|$)/);
 
-	if (version) {
+	if (version)
 		path = 'http://code.jquery.com/jquery-' + version[1] + '.js';
-	}
 
 	document.write('<script src="' + path + '"></script>');
 }());
