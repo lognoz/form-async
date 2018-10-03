707cc363acd1e022a2d62223e0bfd322410945df
lognoz <developer@lognoz.org>
2018-10-03 14:35:23 -0400

    Build: Add wrapper.js

---
+++ src/wrapper.js
@@ -0,0 +1,20 @@
+/*!
+ * JavaScript Autosave v<%= version %>
+ * https://github.com/lognoz/js-autosave
+ *
+ * Copyright 2018, <%= years %> Marc-Antoine Loignon
+ * Released under the MIT license
+ */
+( function( factory ) {
+	if ( typeof define === 'function' && define.amd ) {
+		define( [ 'jquery' ], factory );
+	} else if ( typeof exports === 'object' ) {
+		module.exports = factory( require( 'jquery' ) );
+	} else {
+		factory( jQuery );
+	}
+} )( function( $ ) {
+	'use strict';
+
+	// <%= code %>
+} );
