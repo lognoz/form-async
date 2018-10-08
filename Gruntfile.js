834bbf1b639cfe3a364232a978928f3cf55418e4
lognoz <developer@lognoz.org>
2018-10-08 19:49:20 -0400

    Build: Use src/async.js as build config instead of src/core.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,10 @@ module.exports = function( grunt ) {
 	require( "load-grunt-tasks" )( grunt );
 
 	grunt.config( "build", {
-		"src/core.js": "dist/form-async.js"
+		"src/async.js": "dist/form-async.js"
 	} );
 
 	grunt.loadTasks( "build/tasks" );
 	grunt.registerTask( "dev", [ "build", "dist" ] );
-	grunt.registerTask( "default", [ "dev", "test" ] );
+	grunt.registerTask( "default", [ "dev" ] );
 };
