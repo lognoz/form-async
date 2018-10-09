ef71b9348c7f70cf80c085e01a0bf1839d4fec27
lognoz <developer@lognoz.org>
2018-10-09 13:00:45 -0400

    Build: Replace single quote by double quotes

--- src/event.js
+++ src/event.js
@@ -24,17 +24,17 @@ define( [
 
 				$( target )
 					.on( attr.handler( target ), self.save.bind( self ) )
-					.data( data( 'elements' ), self.elements.length - 1 )
-					.data( data( 'state' ), attr.state( target ) );
+					.data( data( "elements" ), self.elements.length - 1 )
+					.data( data( "state" ), attr.state( target ) );
 			} );
 		},
 		save: function( event ) {
 			var target = event.target,
-				id = $.data( target, data( 'elements' ) ),
+				id = $.data( target, data( "elements" ) ),
 				element = this.elements[ id ],
 				state = attr.state( target );
 
-			if ( $.data( target, data( 'state' ) ) !== state ) {
+			if ( $.data( target, data( "state" ) ) !== state ) {
 				return new Request( {
 					action: element.action || this.action,
 					attribute: element,
