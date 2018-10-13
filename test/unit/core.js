f03871f63150d004cbe554f44675248b00b8dd25
lognoz <developer@lognoz.org>
2018-10-13 13:45:09 -0400

    Tests: Change modules name

--- test/unit/core.js
+++ test/unit/core.js
@@ -5,7 +5,7 @@ define( [
 	"unit/module",
 	"async"
 ], function( $, QUnit, sinon, module ) {
-	QUnit.module( "core - extensions", module );
+	QUnit.module( "core: extensions", module );
 
 	QUnit.test( "$.async", function( assert ) {
 		assert.expect( 1 );
@@ -19,7 +19,7 @@ define( [
 		assert.ok( $.fn.async !== undefined );
 	} );
 
-	QUnit.module( "core - attributes", module );
+	QUnit.module( "core: attributes", module );
 
 	QUnit.test( "async", function( assert ) {
 		assert.expect( 11 );
