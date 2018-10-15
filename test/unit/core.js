42faa591da9987f3561567e054b1824dcdc191fa
lognoz <developer@lognoz.org>
2018-10-14 21:13:17 -0400

    Tests: Refactor core tests and add test cases on markup structure

--- test/unit/core.js
+++ test/unit/core.js
@@ -4,63 +4,101 @@ define( [
 	"sinon",
 	"async"
 ], function( $, QUnit, sinon ) {
-	QUnit.module( "core: extensions" );
 
-	QUnit.test( "$.async", function( assert ) {
-		assert.expect( 1 );
+	QUnit.module( "core" );
 
-		assert.ok( $.async !== undefined );
+	QUnit.test( "basic requirement", function( assert ) {
+		assert.expect( 2 );
+		assert.ok( $.async !== undefined, "$: async" );
+		assert.ok( $.fn.async !== undefined, "$: fn.async" );
 	} );
 
-	QUnit.test( "$.fn.async", function( assert ) {
-		assert.expect( 1 );
+	$.each( {
+		"unique field": {
+			"selector": $( ".exemple" )[ 0 ],
+			"action": "/action/unique-field.html",
+			"elements": 1
+		},
+		"multiple fields": {
+			"selector": $( ".exemple" )[ 1 ],
+			"action": "/action/multiple-fields.html",
+			"elements": 2
+		},
+		"multiple fields with overwriting": {
+			"selector": $( ".exemple" )[ 2 ],
+			"action": "/action/multiple-fields.html",
+			"elements": 2
+		},
+		"checkbox": {
+			"selector": $( ".exemple" )[ 3 ],
+			"action": "/action/checkbox.html",
+			"elements": 2
+		},
+		"radio": {
+			"selector": $( ".exemple" )[ 4 ],
+			"action": "/action/radio.html",
+			"elements": 3
+		},
+		"select": {
+			"selector": $( ".exemple" )[ 5 ],
+			"action": "/action/select.html",
+			"elements": 1
+		},
+		"select with multiple attribute": {
+			"selector": $( ".exemple" )[ 6 ],
+			"action": "/action/select-multiple.html",
+			"elements": 1
+		},
+		"group": {
+			"selector": $( ".exemple" )[ 7 ],
+			"action": "/action/group.html",
+			"elements": 2
+		},
+		"contenteditable": {
+			"selector": $( ".exemple" )[ 8 ],
+			"action": "/action/contenteditable.html",
+			"elements": 1
+		},
+		"complexe name": {
+			"selector": $( ".exemple" )[ 9 ],
+			"action": "/action/complexe-name.html",
+			"elements": 3
+		},
+		"disabled": {
+			"selector": $( ".exemple" )[ 10 ],
+			"action": "/action/disabled.html",
+			"elements": 0
+		}
+	}, function( type, options ) {
+		QUnit.test( "markup structure: " + type, function( assert ) {
+			assert.expect( 3 + ( options.elements * 2 ) );
+			var selector = $( options.selector ),
+				construct = selector.async(),
+				async = selector.data( "async" );
 
-		assert.ok( $.fn.async !== undefined );
-	} );
+			assert.equal( async.form, options.selector );
+			assert.equal( async.action, options.action );
+			assert.equal( async.elements.length, options.elements );
 
-	QUnit.module( "core: attributes" );
-
-	QUnit.test( "async", function( assert ) {
-		assert.expect( 11 );
-
-		$.each( $( ".exemple" ), function( index, target ) {
-			assert.ok( $( target ).data( "async" ) !== undefined );
+			$.each( async.elements, function( index, options ) {
+				selector = $( options.selector );
+				assert.equal( selector.data( "async-element" ), index );
+				assert.ok( selector.data( "previous-state" ) !== undefined );
+			} );
 		} );
 	} );
 
-	QUnit.test( "async-element", function( assert ) {
-		assert.expect( 23 );
-
-		var elements = {
-			"simple-field": true,
-			"multiple-fields-name": true,
-			"multiple-fields-phone": true,
-			"overwrite-action-city": true,
-			"overwrite-action-province": true,
-			"checkbox-bike": true,
-			"checkbox-car": true,
-			"radio-male": true,
-			"radio-female": true,
-			"radio-other": true,
-			"select-car": true,
-			"select-multiple-car": true,
-			"group-password": true,
-			"group-redirection": true,
-			"contenteditable": true,
-			"checkbox-complexe-name-bike": true,
-			"checkbox-complexe-name-car": true,
-			"checkbox-complexe-name-walk": true,
-			"address-disabled-field": false,
-			"city-disabled-field": false,
-			"reset-disabled-field": false,
-			"submit-disabled-field": false,
-			"image-disabled-field": false
-		};
-
-		$.each( elements, function( target, expected ) {
-			assert.ok(
-				( $( "#" + target ).data( "async-element" ) !== undefined ) === expected
-			);
+	$.each( {
+		"disabled attribute": $( "#address-disabled-field" ),
+		"disabled async attribute": $( "#city-disabled-field" ),
+		"reset input": $( "#reset-disabled-field" ),
+		"submit input": $( "#submit-disabled-field" ),
+		"image input": $( "#image-disabled-field" )
+	}, function( type, selector ) {
+		QUnit.test( "disabled inputs: " + type, function( assert ) {
+			assert.expect( 2 );
+			assert.equal( selector.data( "async-element" ), undefined );
+			assert.equal( selector.data( "previous-state" ), undefined );
 		} );
 	} );
 } );
