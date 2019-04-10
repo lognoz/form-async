8abf2c2ffcec994dc537b7855ec2159a28b7d39c
Marc-Antoine Loignon <developer@lognoz.org>
2019-04-10 18:45:24 -0400

    Tests: Remove statement to start qunit in module.js

--- test/unit/module.js
+++ test/unit/module.js
@@ -4,11 +4,6 @@ define( [
 	"sinon",
 	"async"
 ], function( $, QUnit, sinon ) {
-	if ( started !== true ) {
-		QUnit.start();
-		started = true;
-	}
-
 	return {
 		beforeEach: function() {
 			async = $( ".exemple" ).async();
