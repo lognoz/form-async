ea766ee1d2ee7e5ceddea8495c9c3ddf77766869
Marc-Antoine Loignon <developer@lognoz.org>
2019-04-10 17:39:59 -0400

    Build: Refactor callback function

--- src/var/callback.js
+++ src/var/callback.js
@@ -4,6 +4,9 @@ define( [
 	"use strict";
 
 	return function( type, callbacks ) {
-		return isFunction( callbacks[ type ] ) ? callbacks[ type ] : new Function();
+		return isFunction( callbacks[ type ] ) ? callbacks[ type ] : (
+			$.async.defaults !== undefined && isFunction( $.async.defaults[ type ] ) ?
+				$.async.defaults[ type ] : new Function()
+		);
 	};
 } );
