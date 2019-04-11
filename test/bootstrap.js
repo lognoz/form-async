306aabe87a90cad3aa49c76e58cf3bf4df2ccf70
Marc-Antoine Loignon <developer@lognoz.org>
2019-04-11 00:21:37 -0400

    Tests: Change test modules loader in bootstrap.js

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,28 +1,23 @@
 ( function() {
 	requirejs.config( {
-		baseUrl: "../../../src",
+		baseUrl: "../src",
 		paths: {
 			"jquery": "../lib/jquery/jquery",
 			"qunit": "../lib/qunit/qunit",
 			"sinon": "../lib/sinon/sinon",
-			"unit" : "../test/unit"
+			"unit": "../test",
 		}
 	} );
-
-	// Find the script element
-	var scripts = document.getElementsByTagName( "script" );
-	var script = scripts[ scripts.length - 1 ];
-
-	// Read the modules
-	var module = script.getAttribute( "data-modules" );
 
 	// Load test environment
 	require( [ "qunit" ], function( QUnit ) {
 		QUnit.start();
 	} );
 
-	// Load test module based on data attributes
+	// Load test modules
 	require( [
-		"unit/" + module + "/core"
+		"unit/core",
+		"unit/events",
+		"unit/callbacks"
 	] );
 } )();
