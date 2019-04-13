aa5e180ebdd3854cbdcb4d3bf721b8de05f8ec62
Marc-Antoine Loignon <developer@lognoz.org>
2019-04-13 12:19:37 -0400

    Grunt: Register testing, dist and default tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,13 @@
 module.exports = function( grunt ) {
 	require( "load-grunt-tasks" )( grunt );
+	require( "load-grunt-config" )( grunt );
 
 	grunt.config( "build", {
 		"src/async.js": "dist/form-async.js"
 	} );
 
-	grunt.loadTasks( "build/tasks" );
-	grunt.registerTask( "dev", [ "build", "dist" ] );
-	grunt.registerTask( "default", [ "dev", "test" ] );
+	grunt.loadTasks( "grunt" );
+	grunt.registerTask( "testing", [ "eslint", "connect", "qunit" ] );
+	grunt.registerTask( "dist", [ "build", "uglify", "compare_size" ] );
+	grunt.registerTask( "default", [ "dist", "testing" ] );
 };
